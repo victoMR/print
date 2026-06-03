6f2effe804cc9febe4ae24b0beb92f0419ae552a
Arcode <reruaarr@gmail.com>
2026-06-03 20:20:30 +0000

    Ecosystem

---
+++ ecosystem.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: "print-api",
+      cwd: "/home/dir/print/packages/api",
+      script: "dist/index.js",
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "512M",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "print-web",
+      cwd: "/home/dir/print",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: "3000",
+      },
+    },
+  ],
+};
