c21ce764a0d139381efcae786575c09c985a1a45
ArCode <reruaarr@gmail.com>
2026-06-12 19:06:46 -0600

    Build

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Header } from "@/components/boty/header";
 import { Hero } from "@/components/boty/hero";
-import { TrustBadges } from "@/components/boty/trust-badges";
 import { FeatureSection } from "@/components/boty/feature-section";
 import { ProductGrid } from "@/components/boty/product-grid";
 import { Testimonials } from "@/components/boty/testimonials";
@@ -33,11 +32,11 @@ export default async function HomePage() {
       <JsonLd data={websiteJsonLd()} />
       <Header />
       <Hero />
-      <TrustBadges />
       <ProductGrid initialProducts={products} />
-      <FeatureSection />
+      {/* <FeatureSection /> 
       <Testimonials />
       <CTABanner />
+      */}
       <Newsletter />
       <Footer />
       <ScrollToTop />
